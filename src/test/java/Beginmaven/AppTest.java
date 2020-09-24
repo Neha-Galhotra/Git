9d45d20f1d04f5092c14003fca6c3ee1482f2ed9
Neha Galhotra <neha.galhotra1@gmail.com>
2020-09-24 17:29:30 +0530

    first commit

---
+++ src/test/java/Beginmaven/AppTest.java
@@ -0,0 +1,16 @@
+package Beginmaven;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+  
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+       // assertTrue( true );
+    }
+}
