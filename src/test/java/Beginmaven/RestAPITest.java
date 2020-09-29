3e5ee8cacc0a6f67515ffcdae4cd33a5f549ba62
Neha Galhotra <neha.galhotra1@gmail.com>
2020-09-29 13:39:42 +0530

    290920201:38PM commit by y

--- src/test/java/Beginmaven/RestAPITest.java
+++ src/test/java/Beginmaven/RestAPITest.java
@@ -9,6 +9,9 @@ public class RestAPITest {
 		System.out.println("postjira");
 		System.out.println("postjira2");
 		System.out.println("postjira3");
+		System.out.println("postjira4");
+		System.out.println("postjira5");
+		
 	}
 	
 	@Test
